e511a6ac7b9db251720431b75c685f34cbf4a004
prathibha22 <prccherukuri@gmail.com>
2022-12-08 22:36:15 -0500

    implemented methods and all tests passed

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,49 @@
 package com.zipcodewilmington.looplabs;
 
+import java.sql.Array;
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        int counter=0;
+      if(maxNumberOfDuplications<=1) return new Integer[0];
+        StringBuilder sb=new StringBuilder();
+        for( Integer ele:super.array) {
+            for (Integer ele1 : super.array)
+                if (ele == ele1) counter++;
+            if(counter<maxNumberOfDuplications)  sb.append(ele).append(",");
+            counter=0;
+        }
+        String[] outputStringFormat=sb.toString().split(",");
+        Integer[] output=new Integer[outputStringFormat.length];
+        for(int i=0;i<outputStringFormat.length;i++)
+            output[i]=Integer.parseInt(outputStringFormat[i]);
+        return output;
+    }
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int counter=0;
+        StringBuilder sb=new StringBuilder();
+        for( Integer ele:super.array) {
+            for (Integer ele1 : super.array)
+                if (ele == ele1) counter++;
+            if(counter!=exactNumberOfDuplications)  sb.append(ele).append(",");
+            counter=0;
+        }
+        String[] outputStringFormat=sb.toString().split(",");
+        Integer[] output=new Integer[outputStringFormat.length];
+        for(int i=0;i<outputStringFormat.length;i++)
+            output[i]=Integer.parseInt(outputStringFormat[i]);
+        return output;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -5,4 +5,34 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        int counter=0;
+        if(maxNumberOfDuplications<=1) return new String[0];
+        StringBuilder sb=new StringBuilder();
+        for( String ele:super.array) {
+            for (String ele1 : super.array)
+                if (ele == ele1) counter++;
+            if(counter<maxNumberOfDuplications)  sb.append(ele).append(",");
+            counter=0;
+        }
+        return sb.toString().split(",");
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int counter=0;
+        StringBuilder sb=new StringBuilder();
+        for( String ele:super.array) {
+            for (String ele1 : super.array)
+                if (ele == ele1) counter++;
+            if(counter!=exactNumberOfDuplications)  sb.append(ele).append(",");
+            counter=0;
+        }
+        return sb.toString().split(",");
+    }
 }
